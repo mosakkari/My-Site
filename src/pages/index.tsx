13dcb9da251ec54565ed08ac104c512f89aca2d9
Mohamed  SAKKARI <sakkari.tn@gmail.com>
2025-03-11 15:19:40 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <h1>Site en cours de construction</h1>
         </div>
       </div>
     </header>
